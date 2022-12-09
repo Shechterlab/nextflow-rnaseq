c5a790dbccb3e822e7e251b08de01553eca695a0
Maxim Maron <64852004+maximmaron@users.noreply.github.com>
2022-12-09 15:29:10 -0500

    Create 20221207_nextflow_source_file.sh

---
+++ Required_Files_Referenced_to_NGS/20221207_nextflow_source_file.sh
@@ -0,0 +1,13 @@
+#nextflow rnaseq genome reference files
+export nextflow_rna_seq_fasta=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/2230c535660fb4774114bfa966a62f823fdb6d21acf138d4_NOPREFIX.fa
+export nextflow_rna_seq_gtf=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/2230c535660fb4774114bfa966a62f823fdb6d21acf138d4.gtf.gz
+export nextflow_rna_seq_star_index=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/nextflow/index/star/
+export nextflow_rna_seq_rsem_index=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/nextflow/rsem/
+export nextflow_rna_seq_gene_bed=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/nextflow/2230c535660fb4774114bfa966a62f823fdb6d21acf138d4.bed
+export nextflow_rna_seq_transcript_fasta=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/nextflow/genome.transcripts.fa
+
+#nextflow rnaseq DESEQ2 misc scripts
+export reformat_metadata_file=/gs/gsfs0/users/shechter-lab/data/NGS/scripts/maron/rename_nextflow_for_deseq2.awk
+export DESEQ2=/gs/gsfs0/users/shechter-lab/data/NGS/scripts/maron/nextflow_DESEQ2_EnhancedVolcano.R
+export tx2gene=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/tx2gene.csv
+export gtf_gene_annotation_table=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/2230c535660fb4774114bfa966a62f823fdb6d21acf138d4.gtf_gene_annotation_table.txt
