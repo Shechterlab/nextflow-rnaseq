a97886c5e6e0ffd5ee3832a533849da0c4b853d4
Maxim Maron <64852004+maximmaron@users.noreply.github.com>
2022-12-09 15:26:34 -0500

    Create nextflow_rnaseq_test.sh

---
+++ nextflow_rnaseq_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#test nextflow rnaseq pipeline
+
+#SBATCH -p unlimited       #partition/queue name
+#SBATCH --job-name=nextflow_rna    # Job name
+#SBATCH --mail-type=END,FAIL    # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=maxim.maron@einsteinmed.edu # Where to send mail
+#SBATCH --ntasks=1      # Run on a single CPU
+#SBATCH --cpus-per-task=40      # Number of CPU cores per task
+#SBATCH --mem=80gb      # Job memory request
+#SBATCH --time=24:00:00              # Time limit hrs:min:sec
+#SBATCH --output=nextflow_RNA_test.log      # Standard output and error log
+
+source  /gs/gsfs0/hpc01/rhel8/apps/conda3/bin/activate
+conda activate zlib_nextflow_rnaseq
+module load singularity
+module load nextflow
+
+nextflow run nf-core/rnaseq -profile test,singularity --outdir nextflow_test
+
+module unload nextflow
+moedule unload singularity
+conda deactivate
