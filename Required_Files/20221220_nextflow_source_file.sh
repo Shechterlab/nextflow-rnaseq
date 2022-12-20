dfcde1b01c8b75a2fceb42c14db959264f5ce506
Maxim Maron <64852004+maximmaron@users.noreply.github.com>
2022-12-20 18:04:39 -0500

    Update 20221220_nextflow_source_file.sh

--- Required_Files/20221220_nextflow_source_file.sh
+++ Required_Files/20221220_nextflow_source_file.sh
@@ -5,8 +5,8 @@ export nextflow_rna_seq_star_index=/gs/gsfs0/users/shechter-lab/data/NGS/stds/re
 export nextflow_rna_seq_rsem_index=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/nextflow/rsem/
 export nextflow_rna_seq_gene_bed=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/nextflow/2230c535660fb4774114bfa966a62f823fdb6d21acf138d4.bed
 export nextflow_rna_seq_transcript_fasta=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/nextflow/genome.transcripts.fa
-
 #nextflow rnaseq DESEQ2 misc scripts
+export rmats_gtf=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/2230c535660fb4774114bfa966a62f823fdb6d21acf138d4.gtf
 export reformat_metadata_file=~/nextflow-rnaseq-deseq2/Required_Files/rename_nextflow_for_deseq2.awk
 export DESEQ2=~/nextflow-rnaseq-deseq2/Required_Files/nextflow_DESEQ2_EnhancedVolcano.R
 export tx2gene=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/tx2gene.csv
