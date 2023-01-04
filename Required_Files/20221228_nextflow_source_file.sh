4e057722872cf7dd57e7b56262740aec561038f2
Maxim Maron <64852004+maximmaron@users.noreply.github.com>
2023-01-03 21:13:10 -0500

    Update 20221228_nextflow_source_file.sh

--- Required_Files/20221228_nextflow_source_file.sh
+++ Required_Files/20221228_nextflow_source_file.sh
@@ -7,9 +7,9 @@ export nextflow_rna_seq_gene_bed=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refg
 export nextflow_rna_seq_transcript_fasta=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/nextflow/genome.transcripts.fa
 #nextflow rnaseq DESEQ2 misc scripts
 export rmats_gtf=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/2230c535660fb4774114bfa966a62f823fdb6d21acf138d4.gtf
-export rmats_plots=~/nextflow-rnaseq-deseq2/Required_Files/nextflow_R_rmats_plots.R
-export reformat_metadata_file=~/nextflow-rnaseq-deseq2/Required_Files/rename_nextflow_for_deseq2.awk
-export DESEQ2=~/nextflow-rnaseq-deseq2/Required_Files/nextflow_DESEQ2_EnhancedVolcano.R
+export rmats_plots=~/nextflow-rnaseq/Required_Files/nextflow_R_rmats_plots.R
+export reformat_metadata_file=~/nextflow-rnaseq/Required_Files/rename_nextflow_for_deseq2.awk
+export DESEQ2=~/nextflow-rnaseq/Required_Files/nextflow_DESEQ2_EnhancedVolcano.R
 export tx2gene=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/tx2gene.csv
 export gtf_gene_annotation_table=/gs/gsfs0/users/shechter-lab/data/NGS/stds/refgenie/hg38/2230c535660fb4774114bfa966a62f823fdb6d21acf138d4.gtf_gene_annotation_table.txt
-export ClusterProfiler=~/nextflow-rnaseq-deseq2/Required_Files/ClusterProfiler.R
+export ClusterProfiler=~/nextflow-rnaseq/Required_Files/ClusterProfiler.R
