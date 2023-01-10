2fb2163fb3344d55e3ccab175f43e7d4c09482a6
Maxim Maron <64852004+maximmaron@users.noreply.github.com>
2023-01-10 07:55:34 -0500

    Update build_envs.sh

--- build_envs.sh
+++ build_envs.sh
@@ -2,10 +2,10 @@
 
 #This script builds the conda envs and fixes in error within the rmats_nextflow env
 
-conda env create -f ~/nextflow-rnaseq-deseq2/envs/R_nextflow_rnaseq.yml
+conda env create -f ~/nextflow-rnaseq/envs/R_nextflow_rnaseq.yml
 
-conda env create -f ~/nextflow-rnaseq-deseq2/envs/zlib_nextflow_rnaseq.yml
+conda env create -f ~/nextflow-rnaseq/envs/zlib_nextflow_rnaseq.yml
 
-conda env create -f ~/nextflow-rnaseq-deseq2/envs/rmats_nextflow.yml
+conda env create -f ~/nextflow-rnaseq/envs/rmats_nextflow.yml
 
 mv ~/.conda/envs/rmats_nextflow/lib/libncurses.so.6 ~/.conda/envs/rmats_nextflow/lib/libncurses.so.5
